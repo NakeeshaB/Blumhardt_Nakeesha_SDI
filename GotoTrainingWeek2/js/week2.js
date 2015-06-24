8eb7e80a6edec2f0d68fb1237861ae58ecb2b177
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-24 09:08:18 -0400

    Calculated Age
    Calculated Age

--- GotoTrainingWeek2/js/week2.js
+++ GotoTrainingWeek2/js/week2.js
@@ -15,3 +15,20 @@ var name = prompt("Let's figure out how old you are!\nPlease type in your name."
 //Use the varibale and say hello
 alert("Welcome "+ name+"!");
 
+// Ask the user what year they were born
+var yearBorn = prompt("What year were you born?");
+
+//console.log the answer to check it.
+console.log(yearBorn);
+
+//Calculate their age
+
+// We need to know the current year
+//Hardcoded - means type in a specific value.
+var currentYear = 2015;
+
+//figure out the age
+var age = currentYear-yearBorn - 1;
+
+// alert the user with their age
+alert(name+" you are "+ age +" years old.");

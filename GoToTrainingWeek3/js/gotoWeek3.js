0d2490ed9b047f3e520a56815b2b4bde46ac1daf
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 18:10:42 -0400

    Created first conditional test for a pear
    Created first conditional test for a pear

--- GoToTrainingWeek3/js/gotoWeek3.js
+++ GoToTrainingWeek3/js/gotoWeek3.js
@@ -29,7 +29,25 @@ GoToTrainingWeek 3
 
  //Create a variable that will keep track of many pear there are.
 
- var totalNumPears = 0
+ var totalNumPears = 0;
+
+ //Test each item in the array and check if it is a pear
+
+ // Basic structure of a conditonal
+ // if(condition to test){cod that will run if true}If false the code will not run and will skip it
+ //"7"== 7 - true
+ //"7"=== 7 - false
+ //prompts always return  text strings!!
+
+ if(bowlOfFruit[0])=== "pear"){
+   //This code will run if true
+   // Add 1 to the total number of pears
+   console.log("This item is a pear!");
+     totalNumPears++;//totalNumPears =totalNumPears + 1;
+ } else {
+     console.log("This fruit is not a pear.");
+
+ }
 
 
 

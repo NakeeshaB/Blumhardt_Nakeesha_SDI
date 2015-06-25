9cd2e9e4dcb8bf819ae2470a024fa1c81743979c
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 17:50:07 -0400

    Created a fruit array
    Created a fruit array

--- GoToTrainingWeek3/js/gotoWeek3.js
+++ GoToTrainingWeek3/js/gotoWeek3.js
@@ -7,3 +7,29 @@ GoToTrainingWeek 3
 
  //alert("Testing to see if connected.");
 
+ //Create an array of fruit names and then sort it
+
+ //Array- variable that holds multiple variables
+
+ //Index # of an array always starts with 0!
+
+ var bowlOfFruit = ["apple","banana","pear","peach"];
+
+ //console.log(bowlOfFruit);
+
+ console.log(bowlOfFruit[2]);
+
+ //How many iatems are in an array?
+
+ //length property-access this by using dot syntax
+ //fancy name for use a period
+ console.log(bowlOfFruit.length);
+
+ //Count the number of pears in our fruit bowl
+
+ //Create a variable that will keep track of many pear there are.
+
+ var totalNumPears = 0
+
+
+

2cbf0e8b7857fbadafa1f0ad926a30fef3e2ec60
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 11:24:05 -0400

    Connection Test
    Tested the connection between my html and.js file.  Test was successful.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/Blumhardt_Nakeesha_Expressions_Assignment.js
+++
@@ -1,6 +0,0 @@
-/*Nakeesha Blumhardt
- SDI Section #2
- Blumhardt_Nakeesha_Expressions_Assignment
- 6-25-2015
-
- */
---
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
@@ -0,0 +1,9 @@
+/*
+Nakeesha Blumhardt
+SDI Section #2
+Expressions Assignment
+6-25-15
+ */
+
+alert("Test to see if connected.");
+

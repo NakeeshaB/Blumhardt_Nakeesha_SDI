1c995bbfb7e389fa65fd4a0024839e308648f510
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 12:12:30 -0400

    First Console.log check
    The user’s name was successfully  log into the console.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
@@ -9,7 +9,15 @@ Expressions Assignment
 //Create a childcare calculator
 
 //Ask the user if they want to calculate the price of childcare and their name
-var name=prompt("Do you know how much you spend on childcare? Let me help you figure it out." +
-    "\nPlease type in your name.");
+var name=prompt("Do you know how much you spend on childcare?   " + "Let me help you figure it out. " +
+    " \nPlease type in your name.");
+
+// Use the variable and say hello
+alert("Hi "+name+  ", Let's get started");
+
+//console.log user name
+console.log(name)
+
+
 
 

7840ae08de3085a00bd575d35fdbf9bbca15a11d
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 11:49:13 -0400

    First prompt to user
    First prompt to user.  Please enter your name.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
@@ -4,6 +4,12 @@ SDI Section #2
 Expressions Assignment
 6-25-15
  */
+//alert("Test to see if connected.");
 
-alert("Test to see if connected.");
+//Create a childcare calculator
+
+//Ask the user if they want to calculate the price of childcare and their name
+var name=prompt("Do you know how much you spend on childcare? Let me help you figure it out." +
+    "\nPlease type in your name.");
+
 

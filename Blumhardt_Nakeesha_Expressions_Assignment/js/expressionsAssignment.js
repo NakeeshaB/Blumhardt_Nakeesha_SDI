8c09ceb5d7c8470c30566e74b540e08334d80f65
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 15:44:56 -0400

    Calculated Monthly Cost
    Calculated monthly cost of childcare.  Reported back to user.

--- Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
+++ Blumhardt_Nakeesha_Expressions_Assignment/js/expressionsAssignment.js
@@ -19,12 +19,27 @@ alert("Hi "+name+  ", Let's get started");
 console.log(name)
 
 //Ask the user to select the number of children they have or expected to have in childcare
-prompt(name+ ",how many children do you expect to have in childcare?");
+var kids = prompt(name+ " ,how many children do you expect to have in childcare?");
+
 
 //Arrays
 var numberofChildren = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"];
 console.log(numberofChildren[2]);
 
 //figure out weekly cost for each child
-var costperweek= prompt
-("What is the estimated cost per week per child?");
+var costperweek = prompt ("What is the estimated cost per week per child?");
+
+//Calculate the price per week times number of children selected for user.
+
+var weeklyTotal = Number(kids)* Number(costperweek);
+
+//alert the user of weekly childcare cost
+alert(name + " ,you pay an estimated amount of $"+Number(weeklyTotal)+ " for weekly childcare.");
+
+// Calculate monthly cost of child car
+
+var monthlycost = Number(kids)* Number(costperweek)*4;
+//alert the user of monthly cost
+alert(name+", you pay an estimated amount of $ "+Number(monthlycost)+" per month for childcare.");
+
+console.log(monthlycost);

88dd296c780c39e27d9045f9a210b6376e18fd89
Nakeesha Blumhardt <ns_blumhardt@yahoo.com>
2015-06-25 20:37:32 -0400

    Entered variables
    Entered several variables and check to see if they were all working
    properly before entering equations.

--- Blumhardt_Nakeesha_Conditionals_Assignment/js/conditonalsAssignment.js
+++ Blumhardt_Nakeesha_Conditionals_Assignment/js/conditonalsAssignment.js
@@ -6,4 +6,33 @@ Conditionals Assignment
 6-25-15
  */
 
- alert("Testing to see if connected.");
+ //alert("Testing to see if connected.")
+
+ //Greet user and ask for their name
+
+ var name = prompt("Good After noon, may I ask your name? " + name);
+
+ //Alert user that you are going to play quiz game
+
+ alert( name+  "  ,We are going to play a little quiz game.  Enter your answer after the questions.");
+
+ var quiz1 = prompt("If two people in a room shakes hands with every " +
+     "person once, how many total handshakes happen?");
+
+ var quiz2 = prompt("How many states are in the United States?");
+
+ var quiz3 = prompt("How many of the states names end with an A ?");
+
+ var quiz4 = prompt("How many of the states names end with an O ?");
+
+ var quiz5 = prompt("How many quarters make up a dollar?");
+
+ var quiz6 = prompt("On my way to St. Ives I saw a man with 7 wives.Each wife had 7 sacks. " +
+     "Each sack had 7 cats.Each cat had 7 kittens. Kitten, cats, sacks, wives. " +
+     "How many were going to St. Ives?");
+ var quiz7 =prompt("This famous show on TLC has how many kids and counting?");
+
+
+
+
+
